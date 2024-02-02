a5e597b21f0f3ac83a75a26e6106767a33daf339
ehian-alt <1761445546@qq.com>
2024-02-02 15:17:37 +0800

    tabbar

---
+++ src/pages/forum/forum.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="forum">forum</div>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style scoped></style>
---
+++ src/pages/recommend/recommend.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="recommend">recommend</div>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style scoped></style>
