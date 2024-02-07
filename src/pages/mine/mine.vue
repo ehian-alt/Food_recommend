a0a2c5d1329894bc3145dd36a04de2eeb243834f
ehian-alt <1761445546@qq.com>
2024-02-07 20:00:37 +0800

    前后端正常

--- src/pages/mine/mine.vue
+++ src/pages/mine/mine.vue
@@ -1,36 +1,10 @@
 <script lang="ts" setup>
-import { useuserStore } from '@/stores/user'
-import type { useSSRContext } from 'vue';
-const userStore = useuserStore();
-import { http } from '@/utils/http'
-const getData = async () => {
-  let res = await http({
-    method: 'GET',
-    url: '/drugAll',
-  })
-  console.log(res)
-}
-
+import test from './components/test/test.vue'
+import login from './components/login/login.vue'
 </script>
 
 <template>
-  <view class="mine">
-    <view>
-      info:{{ userStore.userInfo }}
-    </view>
-    <button style="border: 1px solid #000000;" @tap="
-      userStore.setUserInfo({
-        token: '2323232',
-        name: 'namename'
-      })
-      " size="mini" type="primary">
-      保存</button>
-    <button style="border: 1px solid red;" size='mini' hover-class="button-hover" @tap="userStore.clearUserInfo">
-      清除
-    </button>
-    <button size="mini" type="primary" @tap="getData">
-      测试网络请求
-    </button>
-  </view>
+  <test></test>
+  <login></login>
 </template>
 <style scoped></style>
